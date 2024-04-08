9bec5d35ca488bf189aeefb068f218fe7d64ca84
Ben <smithbenjamin@ufl.edu>
2024-04-08 12:04:25 -0400

    backprop for input matrix

--- src/neuron.h
+++ src/neuron.h
@@ -17,27 +17,33 @@ struct Neuron {
     Neuron() {}
 
     Matrix<Dval> ForwardProp(const Matrix<Dval>& inputs) {
-        // weights_ is a row vector, inputs need to be columns
+        // weights_ is a row vector, input needs to be a column vector or
+        // a matrix with columns as inputs
         out_ = weights_ * inputs + bias_;
         out_ = out_.Max(0).Transpose();
         return out_;
     }
 
     void BackProp(const Matrix<Dval>& inputs) {
-        bias_.grad_ = out_.ColwiseAverage()[0].grad_;
+        // compute derivatives
         for (size_t i = 0; i < inputs.row_count_; i++) {
+            bias_.grad_ += out_[i].grad_;
             for (size_t j = 0; j < inputs.col_count_; j++) {
                 weights_[i].grad_ += out_[i].grad_ 
                     * inputs[i * inputs.row_count_ + j].grad_;
             }
         }
 
-        bias_.value_ -= bias_.grad_ * learning_rate_ ;
-        for (auto& element_ : weights_.elements_) {
-            element_.value_ -= element_.grad_ * learning_rate_
+        // average derivatives
+        bias_.value_ -= bias_.grad_ * learning_rate_ 
+            * (1 / static_cast<double>(out_.row_count_));
+        for (auto& weight_ : weights_.elements_) {
+            weight_.value_ -= weight_.grad_ * learning_rate_
                 * (1 / static_cast<double>(inputs.col_count_));
         }
     }
+
+    void BackProp(std::vector<Neuron>& input) {}
 
     void ZeroGrad() {
         for (auto& element_ : out_.elements_) element_.grad_ = 0;
